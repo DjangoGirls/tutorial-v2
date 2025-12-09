9981de83102cc3a173c391042ffd983cb3c32a0a
Anna Makarudze <amakarudze@gmail.com>
2025-12-09 22:58:55 +0100

    Add installation instructions for Windows

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   output: 'static',
   site: 'https://tutorial-v2.djangogirls.org/',
   base: '/',
+  integrations: [mdx()],
+  vite: {
+  resolve: {
+    alias: {
+      '@config': '/src/config',
+    },
+  },
+},
+markdown: {
+  shikiConfig: {
+    themes: {
+      light: 'github-light',
+      dark: 'github-dark',
+    },
+  },
+}
 });
